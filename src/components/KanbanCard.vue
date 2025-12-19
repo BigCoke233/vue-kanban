859fad76c781d2db8c8c24ead2ca5b56d3d76c08
Eltrac <hi@guhub.cn>
2025-12-19 23:12:36 +0800

    chore: remove unused default card data

--- src/components/KanbanCard.vue
+++ src/components/KanbanCard.vue
@@ -2,20 +2,7 @@
 import { Tag, Clock, BellElectric } from 'lucide-vue-next'
 const emit = defineEmits(['dragstart', 'dragend'])
 const props = defineProps({
-  data: {
-    type: Object,
-    required: true,
-    default: () => ({
-      id: 1,
-      title: 'Dolor sit amet consectetur adipiscing elit quisque faucibus ex sapien.',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      status: '',
-      priority: '',
-      due: '2025-12-31',
-      created: '2025-12-12',
-      tags: ['inbox', 'task']
-    })
-  }
+  data: Object
 })
 
 function startDrag(e) {
